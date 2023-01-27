d0fc4b1ff90338fde2c15b151cc960d86173a62d
JHKIMS <wjdgns5131@gmail.com>
2023-01-27 23:15:53 +0900

    Framer-motion: Variant을 사용한 코드정리

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Box = styled(motion.div)`
   width: 200px;
@@ -15,16 +15,18 @@ const Box = styled(motion.div)`
   background-color: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
-`
+`;
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
 
-function App(){
+function App() {
   return (
     <Wrapper>
-      <Box
-       transition={{type:"spring", stiffness: 10}}
-       initial={{scale:0}} animate={{scale:1, rotateZ: 360}} />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
-  )
+  );
 }
 
 export default App;
