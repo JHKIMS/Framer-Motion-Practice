f0911efda79c4ab60787d8a1862fff3acb518349
JHKIMS <wjdgns5131@gmail.com>
2023-01-28 10:29:04 +0900

    Framer-motion: MotionValue-x값 추적하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -9,15 +9,7 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(116, 185, 255, 0.5);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -26,28 +18,16 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgba(129, 236, 236,1.0)", transition: { duration: 5 } },
-};
+const boxVariants = {};
 
 function App() {
-  const biggerBoxRef= useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  useMotionValueEvent(x, "change", (xValue) => {
+    console.log(xValue);
+  });
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.8}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileDrag="drag"
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
