67d43b962bf91f785f220a3e1e5455b561c55f0f
JHKIMS <wjdgns5131@gmail.com>
2023-01-28 11:13:13 +0900

    Framer-motion: MotionValue로 배경색상 변경하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { motion, useMotionValue, useMotionValueEvent, useTransform } from "frame
 import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238));
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(116, 185, 255, 1);
+  background-color: rgba(223, 230, 233,1.0);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
 `;
@@ -22,14 +23,23 @@ const boxVariants = {};
 
 function App() {
   const x = useMotionValue(0);
-  const resizeBox = useTransform(x, [-800, 0, 800], [2, 1, 0.3]);
-  useMotionValueEvent(resizeBox, "change", (scaleValue) => {
+  const rotate = useTransform(x, [-800, 800], [-360 , 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgba(253, 203, 110,1.0))",
+    ]
+  );
+
+  useMotionValueEvent(rotate, "change", (scaleValue) => {
     // console.log(xValue);
     console.log(scaleValue);
   });
   return (
-    <Wrapper>
-      <Box style={{ x, scale:resizeBox }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{background: gradient}}>
+      <Box style={{ x, rotateZ:rotate }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
