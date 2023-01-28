d231d404dacb4a47645114a98b2801eb2c98f831
JHKIMS <wjdgns5131@gmail.com>
2023-01-28 20:14:31 +0900

    Framer-motion: Box위치 정렬완료

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238));
 `;
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -21,23 +20,26 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: rgba(0, 206, 201, 1);
-  width: 100px;
-  height: 100px;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child{
+    grid-column: span 2;
+  }
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>
-        {!clicked ? <Circle layoutId="circle" style={{borderRadius:50}}/> : null}
-      </Box>
-      <Box>
-        {!clicked? null : <Circle layoutId="circle" style={{borderRadius:0}}/>}
-      </Box>
+    <Wrapper>
+      <Grid>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
     </Wrapper>
   );
 }
